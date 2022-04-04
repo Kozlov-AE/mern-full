ab2a1a9b0c498fe19eb4a81a0f19c05bd41a7c4f
Kozlov-AE <Allek-007@list.ru>
2022-04-04 08:39:23 +0300

    Added config, started work with mongodb

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
 const app = express();
 
-app.listen(5000, () => console.log("App works!!!"));
+//app.use('/api/auth');
+
+const PORT = config.get('port') || 5001;
+
+async function start(){
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => console.log(`App started on port ${PORT}...`));
+    }
+    catch(e) {
+        console.log('Server error', e.message);
+        process.exit(1);
+    }
+}
+
+start();
